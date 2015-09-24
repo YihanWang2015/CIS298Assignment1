7fafafbdc5d298b3ba618a55cacc7123c31c821d
Yihan Wang <ywang4241@mail.kvcc.edu>
2015-09-24 15:57:11 -0400

    CIS298_Assignment_1_Yihan

--- app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
+++ app/src/main/java/edu/kvcc/cis298/cis298assignment1/CounterActivity.java
@@ -4,13 +4,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CounterActivity extends AppCompatActivity {
+
+
+    private Button mPlusButton;
+    private Button mMinusButton;
+    private TextView mCounterTextView;
+    int counter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
+
+        mCounterTextView = (TextView)findViewById(R.id.counter_text);
+        mPlusButton = (Button)findViewById(R.id.plus_button);
+        mPlusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (v == mPlusButton){
+                    counter++;
+                    mCounterTextView.setText(Integer.toString(counter));
+
+                    if(counter == 20){
+                        Toast.makeText(CounterActivity.this,
+                            R.string.reminder_toast,
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }
+        });
+
+        mMinusButton =(Button)findViewById(R.id.minus_button);
+        mMinusButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                if(v == mMinusButton){
+
+                    counter--;
+                    mCounterTextView.setText(Integer.toString(counter));
+                }
+            }
+        });
     }
 
     @Override
